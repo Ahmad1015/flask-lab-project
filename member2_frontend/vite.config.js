e5b3eda28adf22aceb00e10fd1e971acc2d4211d
Ahmad <mahmedraza90@gmail.com>
2025-10-25 07:53:42 +0000

    separated CI CD

--- member2_frontend/vite.config.js
+++ member2_frontend/vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Set base path for GitHub Pages deployment
+  base: process.env.NODE_ENV === 'production' ? '/flask-lab-project/' : '/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
